4e55a1541f5e4477c9627da329336bab86cbe1e2
Ayush Raj <148717357+ayushh002@users.noreply.github.com>
2026-03-22 17:30:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,6 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
